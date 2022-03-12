c2a6d1e84e46d0ca165f013387804d4445f61c73
Ploscar Andreea <aploscar00@gmail.com>
2022-03-12 15:54:19 +0200

    spring hello world

---
+++ iss_project/src/main/java/com/example/iss_project/IssProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.iss_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssProjectApplication.class, args);
+    }
+
+}
---
+++ iss_project/src/test/java/com/example/iss_project/IssProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iss_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
