c2e5b3e71d05d7c4f3cde3de5822ff7c3b8e9a88
alex-nedelcu <alexnedelcu@Alexs-personal-MacBook-Pro.local>
2022-03-21 18:23:15 +0200

    Transform iss_project dependencies into backend dependencies

--- backend/src/main/java/com/example/HelloWorldController.java
+++ backend/src/main/java/com/example/HelloWorldController.java
@@ -1,5 +1,4 @@
-package com.example.iss_project;
-
+package com.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- backend/src/test/java/com/example/IssProjectApplicationTests.java
+++ backend/src/test/java/com/example/IssProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.iss_project;
+package com.example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
