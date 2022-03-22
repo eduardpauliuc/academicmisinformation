50c67fef97c0b5d91d497a0f7f87d920f169aaac
alex-nedelcu <alexnedelcu@Alexs-personal-MacBook-Pro.local>
2022-03-22 20:00:02 +0200

    Add BaseEntity and Faculty models for future reference

--- backend/src/main/resources/db/migration/postgresql/V3__insert_faculties_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO migrations.faculties (name, website)
-VALUES ('Faculty of Mathematics and Computer Science', 'https://cs.ubbcluj.ro'),
-       ('Faculty of Geography', 'https://geografie.ubbcluj.ro');
