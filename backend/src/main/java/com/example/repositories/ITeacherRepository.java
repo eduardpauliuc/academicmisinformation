28a0c65cff5b24728f035a9baa55310af72c2371
alex-nedelcu <alexnedelcu@Alexs-personal-MacBook-Pro.local>
2022-04-08 16:12:49 +0300

    Generate repositories for Course, Specialization, Faculty, StaffMember, Teacher, Student

---
+++ backend/src/main/java/com/example/repositories/ITeacherRepository.java
@@ -0,0 +1,9 @@
+package com.example.repositories;
+
+import com.example.models.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITeacherRepository extends JpaRepository<Teacher, Long> {
+}
